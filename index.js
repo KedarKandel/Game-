a71deb4759ada43980cf29dcef4aa91695f74499
Kedar Kandel <kandelkedar07@gmail.com>
2022-05-05 21:27:54 +0300

    palyer and projectiles created

--- index.js
+++ index.js
@@ -4,47 +4,75 @@ const c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-class Boundry {
-  constructor({ position }) {
-    this.position = position;
-    this.width = 40;
-    this.height = 40;
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
   }
-
   draw() {
-    c.fillStyle = "blue";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+}
+///player at the center
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+const player = new Player(x, y, 30, "blue");
+
+
+//projectiles in diff directions
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
   }
 }
 
-const map = [
-    //todo next... brainstorming
-    // rows and columns
-    //check to see if looping through the rows and symbol associated can create boundry at once,
-    //make switch case for the symbol and define positions using index used to loop...
-    //may be push each Boudndy into boundaries
+const projectiles = [];
 
-]
+// animation frame
 
-// const boundaries =[]
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0,0, canvas.width, canvas.height)
+  player.draw();
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
+animate();
+// push whenever click is detected, find angle from x-axis to the point of click event.
+window.addEventListener("click", (e) => {
+  const angle = Math.atan2(
+    e.clientY - canvas.height / 2,
+    e.clientX - canvas.width/2
+  );
 
-
-
-const boundaries = [
-  new Boundry({
-    position: {
-      x: 0,
-      y: 0,
-    },
-  }),
-  new Boundry({
-    position: {
-      x: 41,
-      y: 0,
-    },
-  }),
-];
-
-boundaries.forEach((boundry) => {
-  boundry.draw();
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)
+  }
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+  );
 });
